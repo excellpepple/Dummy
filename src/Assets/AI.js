e7ff93f1e4073d296762a6fe248bb6dfa70abadd
AnthonyBlazeJackson <94572857+AnthonyBlazeJackson@users.noreply.github.com>
2021-12-04 21:27:32 -0500

    Update AI.js

---
+++ src/Assets/AI.js
@@ -0,0 +1,13 @@
+import {
+    tags as Tags,
+} from '../data/tags'
+import {
+    articles as Articles,
+} from '../global/FetchArticle'
+
+render = [];
+averages = [];
+while (render.length < 5) {
+    Tags.forEach(tag => averages.push(tag.getAverage()));
+}
+console.log(averages)
