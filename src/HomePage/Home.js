228c01edb7e1326d6e91f4f750d9585931c06886
AnthonyBlazeJackson <94572857+AnthonyBlazeJackson@users.noreply.github.com>
2021-12-06 01:49:56 -0500

    Added the AI into home.js and sort of abandoned AI.js

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -10,9 +10,9 @@ import {
 } from '../data/tags'
 // here is the database
 import {GetArticles} from "../data/ArticleDb";
+// import {articles as Articles} from "../global/FetchArticles"
 import ArticlePage from "../ArticlePage/ArticlePage";
-
-
+import {ArticleRender} from "../Assets/AI";
 
 
 
@@ -20,34 +20,82 @@ export default function Home() {
     const [tags, setTags] = useState(Tags)
     const [userTags, setUserTags] = useState([])
     console.log(GetArticles())
-    const updateValues = (tagname, value) =>{
+    const updateValues = (tagname, value) => {
         let message = "";
-        // const shifter = (target) =>{
-        //     if (target.length <= 5){
-        //         console.log(`we just added 1 with no issue`)
-        //         target.value.push(value)
-        //     }
-        //     else {
-
-        //         target.value.shift()
-        //         console.log(`we just shifted the first index of ${target.tag}`)
-        //         target.value.push(value)
-        //         console.log(`we just added a new value ${target.tag}`);
-        //     }
-        // }
-        tags.forEach(tag => tag.tag === tagname? target.value.push(value): message = "Tag can not be found!");
         tags.forEach(tag => {
-            if (tag.value.length > 5) {
+            if (tag.tag === tagname) {
+                tag.value.push(value);
+            } else {
+                message = "Tag can not be found!";
+            }
+            if (tag.value.length > 20) {
                 tag.value.shift()
             }
-        })
-
+        });
         // const update = [...tags[tagname]]
 
         console.log(`updated ${tagname} and added ${value}`)
         return message;
     }
 
+    // const rendered_articles = ArticleRender(tags)
+    // console.log(rendered_articles)
+    // Creates a copy of the articles
+    const Articles = GetArticles()
+    // Creates and offset
+    const offset = 0.1
+
+    const rendered_articles = () => {
+            
+        if(Articles.length === 0 ) return "empty";
+        // Creates an Array to store the chosen articles
+        const render = [];
+        // loops the code 5 times
+        for (let j = 0; j < 15; j++) {
+            // Creates an Array to store the averages from tags IN ORDER
+            const averages = [];
+
+            tags.forEach(tag => console.log(tag));
+            // Tags.forEach(tag => console.log(tag.getAverage()));
+            // Actually assigns the averages to the averages array
+            tags.forEach(tag => {
+                let average = tag.getAverage()
+                console.log(average)
+                averages.push(average)}
+                );
+        
+            // Creates a variable to hold the sum of all of the averages
+            const sum = averages.reduce((a, b)=> a + b)
+        
+            // randomly picks a number between 0 and the sum
+            let pick = Math.random() * (sum + (offset * averages.length))
+        
+            // Checks to see if pick is between 0 and averages[0]
+            if (pick >= 0) {
+                if (pick < (averages[0] + offset)) {
+                    // adds the first article in Article index 0 to render
+                    render.push([Articles[0].pop(), tags[0].tag, tags[0].sub]);
+                } else {
+                    // Iterates through all of the array averages except for averages[0] and averages[-1]
+                    for (let i = 1; i < (averages.length - 1); i++) {
+                        // Checks if the pick is between the sum of all of the averages up to index i and the sum of all of the averages up to index i + 1
+                        if (pick >= (averages.slice(0, i).reduce((a, b) => a + b) + (i * offset)) && pick < (averages.slice(0, i + 1).reduce((a, b) => a + b) + ((i + 1) * offset))) {
+                            // adds the first article in Article index i to render
+                            render.push([Articles[i].pop(), tags[i].tag, tags[i].sub]);
+                            // Breaks out of the for loop
+                            break;
+                        }
+                    }
+                    // Checks to see if pick is above the sum of all of the averages up to index -1
+                    if (pick >= averages.slice(0, -1).reduce((a, b) => a + b) + ((averages.length - 1) * offset)) {
+                        // adds the first article in Article index -1 to render
+                        render.push([Articles[Articles.length - 1].pop(), tags[tags.length - 1].tag, tags[tags.length - 1].sub]);
+                    }
+                }
+            }
+        }
+        return render;
+    }
 
     return (
         <BrowserRouter basename="/">
@@ -69,6 +117,7 @@ export default function Home() {
                   }
                 />
                 </Routes>
+
             </main>
 
 
