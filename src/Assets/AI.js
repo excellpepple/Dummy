ce6ccb62be447f9b838cf1f00cc90c47d8105b7f
Excell Pepple <49886472+excellpepple@users.noreply.github.com>
2021-12-05 21:21:44 -0500

    ai edited

--- src/Assets/AI.js
+++ src/Assets/AI.js
@@ -8,7 +8,7 @@ import {
 } from '../global/FetchArticle'
 
 // Creates a copy of the articles
-Articles = [...A]
+const Articles = [...A]
 
 
 // returns nw sets of articles each timeits called
@@ -20,11 +20,11 @@ function articleRender () {
     const render = [];
     // loops the code 5 times
     while (render.length < 5) {
-        // Acually assigns the averages to the averages array
+        // Actually assigns the averages to the averages array
         Tags.forEach(tag => averages.push(tag.getAverage()));
 
         // Creates a variable to hold the sum of all of the averages
-        const sum = averages.reduce()
+        const sum = averages.reduce((a, b)=> a + b)
 
         // randomly picks a number between 0 and the sum
         let pick = Math.random() * sum
@@ -37,7 +37,7 @@ function articleRender () {
             // Iterates through all of the array averages except for averages[0] and averages[-1]
             for (let i = 1; i < (averages.length - 1); i++) {
                 // Checks if the pick is between the sum of all of the averages up to index i and the sum of all of the averages up to index i + 1
-                if (pick >= averages.slice(0,i).reduce && pick >= averages.slice(0,i + 1).reduce) {
+                if (pick >= averages.slice(0,i).reduce((a,b) => a + b) && pick >= averages.slice(0,i + 1).reduce((a,b) => a + b)) {
                     // adds the first article in Article index i to render
                     render.push([Articles[i][0].pop(), Tags[i].tag]);
                     // Breaks out of the for loop
--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -19,66 +19,6 @@ export default function Home() {
     const [tags, setTags] = useState(Tags)
     const [userTags, setUserTags] = useState([])
 
-    // Creates an Array to store the chosen articles
-    const render = [];
-    // Creates an Array to store the averages from tags IN ORDER
-    const averages = [];
-    const offset = 0.1;
-
-    // loops the code 5 times
-    // while (render.length < 5) {
-    //     // Actually assigns the averages to the averages array
-    //     Tags.forEach(tag => averages.push(tag.getAverage()));
-    //
-    //     // Creates a variable to hold the sum of all of the averages
-    //     const sum = averages.reduce((a, b)=> a + b)
-    //
-    //     // randomly picks a number between 0 and the sum
-    //     let pick = Math.random() * sum
-    //
-    //     // Checks to see if pick is between 0 and averages[0]
-    //     if (pick >= 0) {
-    //         if (pick < (averages[0] + offset)) {
-    //             // adds the first article in Article index 0 to render
-    //             render.push(Articles[0][0].pop())
-    //         } else {
-    //             // Iterates through all of the array averages except for averages[0] and averages[-1]
-    //             for (let i = 1; i < (averages.length - 1); i++) {
-    //                 // Checks if the pick is between the sum of all of the averages up to index i and the sum of all of the averages up to index i + 1
-    //                 if (pick >= (averages.slice(0, i).reduce((a, b) => a + b) + (i * offset)) && pick >= (averages.slice(0, i + 1).reduce((a, b) => a + b)) + ((i + 1) * offset)) {
-    //                     // adds the first article in Article index i to render
-    //                     render.push(Articles[i][0].pop());
-    //                     // Breaks out of the for loop
-    //                     break;
-    //                 }
-    //             }
-    //             // Checks to see if pick is above the sum of all of the averages up to index -1
-    //             if (pick > Articles.slice(0, -1) + ((averages.length - 1) * offset)) {
-    //                 // adds the first article in Article index -1 to render
-    //                 render.push(Articles[-1][0].pop());
-    //             }
-    //         }
-    //     } else {
-    //         // Iterates through all of the array averages except for averages[0] and averages[-1]
-    //         for (let i = 1; i < (averages.length - 1); i++) {
-    //             // Checks if the pick is between the sum of all of the averages up to index i and the sum of all of the averages up to index i + 1
-    //             if (pick >= (averages.slice(0, i).reduce((a, b) => a + b) + (i * offset)) && pick >= (averages.slice(0, i + 1).reduce((a, b) => a + b)) + ((i + 1) * offset)) {
-    //                 // adds the first article in Article index i to render
-    //                 render.push(Articles[i][0].pop());
-    //                 // Breaks out of the for loop
-    //                 break;
-    //             }
-    //         }
-    //         // Checks to see if pick is above the sum of all of the averages up to index -1
-    //         if (pick > Articles.slice(0, -1) + ((averages.length - 1) * offset)) {
-    //             // adds the first article in Article index -1 to render
-    //             render.push(Articles[-1][0].pop());
-    //         }
-    //     }
-    // }
-
-
-
     const updateValues = (tagname, value) =>{
         let message = "";
 
