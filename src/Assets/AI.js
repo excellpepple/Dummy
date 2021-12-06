77579c2bc1b350b9cb6b0532e4295514c6a76242
AnthonyBlazeJackson <94572857+AnthonyBlazeJackson@users.noreply.github.com>
2021-12-05 21:29:45 -0500

    Update AI.js

--- src/Assets/AI.js
+++ src/Assets/AI.js
@@ -9,6 +9,8 @@ import {
 
 // Creates a copy of the articles
 const Articles = [...A]
+// Creates and offset
+const offset = 0.1
 
 
 // returns nw sets of articles each timeits called
@@ -22,32 +24,34 @@ function articleRender () {
     while (render.length < 5) {
         // Actually assigns the averages to the averages array
         Tags.forEach(tag => averages.push(tag.getAverage()));
-
+    
         // Creates a variable to hold the sum of all of the averages
         const sum = averages.reduce((a, b)=> a + b)
-
+    
         // randomly picks a number between 0 and the sum
         let pick = Math.random() * sum
-
+    
         // Checks to see if pick is between 0 and averages[0]
-        if (pick >= 0 && pick < averages[0]) {
-            // adds the first article in Article index 0 to render
-            render.push([Articles[0][0].pop(), Tags[0].tag])
-        } else {
-            // Iterates through all of the array averages except for averages[0] and averages[-1]
-            for (let i = 1; i < (averages.length - 1); i++) {
-                // Checks if the pick is between the sum of all of the averages up to index i and the sum of all of the averages up to index i + 1
-                if (pick >= averages.slice(0,i).reduce((a,b) => a + b) && pick >= averages.slice(0,i + 1).reduce((a,b) => a + b)) {
-                    // adds the first article in Article index i to render
-                    render.push([Articles[i][0].pop(), Tags[i].tag]);
-                    // Breaks out of the for loop
-                    break;
+        if (pick >= 0) {
+            if (pick < (averages[0] + offset)) {
+                // adds the first article in Article index 0 to render
+                render.push([Articles[0][0].pop(), Tags[0].tag])
+            } else {
+                // Iterates through all of the array averages except for averages[0] and averages[-1]
+                for (let i = 1; i < (averages.length - 1); i++) {
+                    // Checks if the pick is between the sum of all of the averages up to index i and the sum of all of the averages up to index i + 1
+                    if (pick >= (averages.slice(0, i).reduce((a, b) => a + b) + (i * offset)) && pick >= (averages.slice(0, i + 1).reduce((a, b) => a + b)) + ((i + 1) * offset)) {
+                        // adds the first article in Article index i to render
+                        render.push([Articles[i][0].pop(), Tags[i].tag]);
+                        // Breaks out of the for loop
+                        break;
+                    }
                 }
-            }
-            // Checks to see if pick is above the sum of all of the averages up to index -1
-            if (pick > Articles.slice(0,-1)) {
-                // adds the first article in Article index -1 to render
-                render.push([Articles[-1][0].pop(), Tags[-1].tag]);
+                // Checks to see if pick is above the sum of all of the averages up to index -1
+                if (pick > Articles.slice(0, -1) + ((averages.length - 1) * offset)) {
+                    // adds the first article in Article index -1 to render
+                    render.push([Articles[-1][0].pop(), Tags[0].tag]);
+                }
             }
         }
     }
